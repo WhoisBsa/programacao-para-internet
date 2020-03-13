6e83249eb9436ca813a953a8db6a1565d2145420
WhoisBsa <mbscbsjbs@gmail.com>
2020-03-13 19:17:56 -0300

    Aula 06 - Continuação

---
+++ Aula06-03/script.js
@@ -0,0 +1,8 @@
+for (let index = 1; index <= 10; index++) {
+	document.write(`<div class="ad">
+										<img src="images/computador.png" alt="">
+										<h1>Produto ${index}</h1>
+										<h2>Descrição ${index}</h2>
+										<h3>R$ 1500,00</h3>
+									</div>`);
+}
