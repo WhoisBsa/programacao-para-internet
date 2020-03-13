a189a3608357182bdbb4a638382aead05d1d8f82
WhoisBsa <mbscbsjbs@gmail.com>
2020-03-13 19:43:46 -0300

    Aula13-03

---
+++ Aula13-03/script.js
@@ -0,0 +1,18 @@
+var vetor = [45];
+
+console.log(vetor[0]);
+
+vetor[1] = 32;
+
+console.log(vetor[1]);
+
+vetor.push("Matheus");  // == vertor[2] = "Matheus";
+
+console.log(vetor[2]);
+
+var vetor2 = ["Cidicleyson", 25, Math.PI, 'a'];
+
+for(i=0; i<vetor2.length; i++){
+    console.log(vetor2[i]);
+}
+
